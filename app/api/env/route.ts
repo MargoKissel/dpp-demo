6e5b0a204315da1c3437dfa77999736d774ade4b
Margarita <margo@Mac.fritz.box>
2025-07-25 01:40:25 +0200

    chore: remove env debug

--- app/api/env/route.ts
+++
@@ -1,8 +0,0 @@
-
-export async function GET() {
-  return Response.json({
-    SHEETS_API      : process.env.SHEETS_API,
-    SHEETS_API_KEY  : process.env.SHEETS_API_KEY,
-    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
-  });
-}
