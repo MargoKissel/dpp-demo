dfadca7736c1518ab38c815b817d397b74aaa642
Margarita <margo@Mac.fritz.box>
2025-07-25 10:18:24 +0200

    debug: expose env

---
+++ app/api/env/route.ts
@@ -0,0 +1,7 @@
+export async function GET() {
+  return Response.json({
+    SHEETS_API     : process.env.SHEETS_API,
+    SHEETS_API_KEY : process.env.SHEETS_API_KEY,
+    SITE_URL       : process.env.NEXT_PUBLIC_SITE_URL,
+  });
+}
