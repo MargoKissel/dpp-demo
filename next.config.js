0578406343360688de31a70b420b885bb5745422
MargoKissel <margarita.kisselyova@gmail.com>
2025-07-26 17:24:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['raw.githubusercontent.com', 'res.cloudinary.com', 'drive.google.com'],
+    domains: ['drive.google.com'],
   },
 };
 export default nextConfig;
