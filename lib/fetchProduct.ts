5b75f0a2182b81e2f03e95a6dfa9db226170499a
MargoKissel <margarita.kisselyova@gmail.com>
2025-07-27 00:02:02 +0200

    Update fetchProduct.ts

--- lib/fetchProduct.ts
+++ lib/fetchProduct.ts
@@ -1,10 +1,10 @@
 // lib/fetchProduct.ts
 export async function fetchProduct(sku: string) {
   const SHEETS =
-    process.env.SHEETS_API ??
+    process.env.SHEETS_API https://script.google.com/macros/s/AKfycbzvZ0HtvsTbQCPkUpZBGiGMdm9AKlsWodSZAyOF8aWPnxSfXcHN5MstBPVx1gQEWX26/exec
     process.env.NEXT_PUBLIC_SHEETS_API_BASE; // берём базовый URL откуда есть
   const KEY =
-    process.env.SHEETS_API_KEY ??
+    process.env.SHEETS_API_KEY r9vXh7KdPq6s2LJ4zUt0GmYcEsBnQa5
     process.env.NEXT_PUBLIC_SHEETS_API_KEY; // берём ключ откуда есть
 
   if (!SHEETS || !KEY) {
