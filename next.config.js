f2764a644c16834274e421948623dd914c04dba4
MargoKissel <margarita.kisselyova@gmail.com>
2025-07-27 23:07:21 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['drive.google.com'],
+    // для внешнего рендеринга (Google Drive)
+    domains: [
+      'drive.google.com',
+      'lh3.googleusercontent.com'
+    ],
+    // оставьте unoptimized, если не хотите настраивать loader
+    unoptimized: true,
   },
-};
-export default nextConfig;
+}
+
+export default nextConfig
