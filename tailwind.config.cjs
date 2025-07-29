b7e1c28e46c7554a53bdbdd5a233de6411940776
MargoKissel <margarita.kisselyova@gmail.com>
2025-07-29 13:21:40 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.{js,ts,jsx,tsx}",        // все страницы App Router
+    "./src/app/**/*.{js,ts,jsx,tsx}",     // если у тебя структура /src/app
+    "./components/**/*.{js,ts,jsx,tsx}",  // все компоненты
+    "./lib/**/*.{js,ts}",                 // утилиты (fetchProduct и т.д.)
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
+};
